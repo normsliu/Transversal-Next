99ef9e20a84a84765543af4f8881d6975746085e
Kevin Mun <k.mun94@Hotmail.com>
2022-04-11 22:33:12 -0600

    updated

--- components/Authors.js
+++ components/Authors.js
@@ -1,36 +1,87 @@
-import React from 'react'
-import styles from '../styles/authors.module.css';
+import React from "react";
+import styles from "../styles/authors.module.css";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Authors = () => {
   return (
     <section>
-      <div> 
-        <h2 className={styles.title}>
-        TransversaL Team
-       
-        </h2>
+      <div>
+        <h2 className={styles.title}>TransversaL Team</h2>
       </div>
       <div className={styles.container}>
-    
-  
-        <div>
-          Kevin
+        <div className={styles.social_icons}>
+          <a
+            href="https://github.com/han900204"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/han900204/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          Han
         </div>
-
-        <div>
+        <div className={styles.social_icons}>
+          <a
+            href="https://github.com/daniel-forrester"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/danielforrester/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
           Dan
-       </div>
-       <div>
-         Han
-      </div>
-      <div>
-        Norman
-      </div>
+          </a>
+        
+        </div>
+        <div className={styles.social_icons}>
+          <a
+            href="https://github.com/normsliu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/norm-liu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+            Norman
+          </a>
+       
+        </div>
+        <div className={styles.social_icons}>
+          <a
+            href="https://github.com/kmun94"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/kevin-mun-27070b231"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+     
+        </div>
       </div>
     </section>
-    
-    
-  )
-}
+  );
+};
 
 export default Authors;
