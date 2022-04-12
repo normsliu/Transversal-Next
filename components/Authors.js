63faeb5d7b826a75b0e14583ca8c684fefac0214
normsliu <normanliu91@gmail.com>
2022-04-12 15:22:30 -0400

    updated section layout

--- components/Authors.js
+++ components/Authors.js
@@ -5,12 +5,12 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 const Authors = () => {
   return (
     <section>
-      <div>
         <h2 className={styles.title}>TransversaL Team</h2>
-      </div>
       <div className={styles.container}>
-        <div>
+        <div className={styles.author_container}>
+          <div>PIC</div>
           <div className={styles.social_icons}>
+            Han
             <a
               href="https://github.com/han900204"
               target="_blank"
@@ -25,58 +25,67 @@ const Authors = () => {
             >
               <FaLinkedin />
             </a>
-            Han
           </div>
         </div>
-        <div className={styles.social_icons}>
-          <a
-            href="https://github.com/daniel-forrester"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithub />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/danielforrester/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaLinkedin />
+        <div className={styles.author_container}>
+          <div>PIC</div>
+          <div className={styles.social_icons}>
             Dan
-          </a>
+            <a
+              href="https://github.com/daniel-forrester"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/danielforrester/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+          </div>
         </div>
-        <div className={styles.social_icons}>
-          <a
-            href="https://github.com/normsliu"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithub />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/norm-liu/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaLinkedin />
+        <div className={styles.author_container}>
+          <div>PIC</div>
+          <div className={styles.social_icons}>
             Norman
-          </a>
+            <a
+              href="https://github.com/normsliu"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/norm-liu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+          </div>
         </div>
-        <div className={styles.social_icons}>
-          <a
-            href="https://github.com/kmun94"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithub />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/kevin-mun-27070b231"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaLinkedin />
-          </a>
+        <div className={styles.author_container}>
+          <div>PIC</div>
+          <div className={styles.social_icons}>
+            Kevin
+            <a
+              href="https://github.com/kmun94"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/kevin-mun-27070b231"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+          </div>
         </div>
       </div>
     </section>
